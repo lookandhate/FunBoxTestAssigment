a7834050e151479c80de94398f9003fcb5bfb7b4
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-03-21 19:39:35 +0400

    Completed GET request

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import redis
-from redis import Redis
-from fastapi import FastAPI
-
 import datetime
 import logging
+from urllib.parse import urlparse
 
+from redis import Redis
+from fastapi import FastAPI
 from typing import Optional
 
 from request_models.models import PostRequestBodyModel
@@ -21,16 +20,6 @@ async def startup_event():
     redis_client = Redis(host='localhost', port=6379, db=0)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 @app.post('/visited_links')
 async def visited_links_post(links: PostRequestBodyModel):
     current_unix_time = datetime.datetime.now().timestamp()
@@ -39,15 +28,21 @@ async def visited_links_post(links: PostRequestBodyModel):
 
 
 @app.get('/visited_domains')
-async def visited_domains_get(from_timestamp: int | None, to: int | None):
+async def visited_domains_get(from_timestamp: int | None = None, to: int | None = None):
     from_timestamp = from_timestamp if from_timestamp is not None else 0
     to = to if to is not None else datetime.datetime(year=3000, day=1, month=1).timestamp()
 
-    all_timestamps = redis_client.hkeys('links')
-    filtered_timestamps = list(filter(lambda x: from_timestamp < int(x) < to, all_timestamps))
+    all_timestamps = list(map(lambda x: int(x.decode()), redis_client.hkeys('links')))
+    filtered_timestamps = list(filter(lambda x: from_timestamp <= x <= to, all_timestamps))
 
     links = [redis_client.hget('links', timestamp) for timestamp in filtered_timestamps]
+    links = list(map(lambda x: x.decode().split('^'), links))
 
+    flat_links = [link for sublist in links for link in sublist]
+    print(flat_links)
 
-    return {'domains': str(links).split('^'),
+    # Extract domain from link
+    domains = set([urlparse(link).netloc if urlparse(link).netloc else link for link in flat_links])
+
+    return {'domains': domains,
             'status': 'ok'}
